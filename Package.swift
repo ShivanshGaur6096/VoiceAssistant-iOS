335136a85e6c9ae3ff1179343c828dcf7977da10
Shivansh Gaur <shivansh.gaur@blackngreen.com>
2025-02-25 12:03:55 +0530

    Added SPM file

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "VoiceAssistant",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "VoiceAssistant",
+            targets: ["VoiceAssistant"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/WebRTC-lib/WebRTC.git", from: "130.0.0")
+    ],
+    targets: [
+        .target(
+            name: "VoiceAssistant",
+            dependencies: [
+                .product(name: "WebRTC", package: "WebRTC-lib")
+            ],
+            path: "VoiceAssistant-iOS/VoiceAssistant-iOS/Sources",
+            exclude: [],
+            resources: [],
+            publicHeadersPath: "include",
+            cSettings: [],
+            swiftSettings: [],
+            linkerSettings: [
+                .linkedFramework("Foundation"),
+                .linkedFramework("UIKit"),
+                .linkedFramework("AVFoundation")
+            ]
+        )
+    ]
+)
+
